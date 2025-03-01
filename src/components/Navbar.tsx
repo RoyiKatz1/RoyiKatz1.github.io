0ce59763dbe1113b966129dc7de131fa92ba34e9
RoyiKatz1 <royicats@gmail.com>
2025-03-01 11:16:03 +0200

    download resume

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
 import "../styles/Navbar.css";
+import downloadIcon from "../assets/icons/Download.svg";
 
 const Navbar = () => {
 	return (
 		<nav className="navbar">
-			<a href="/">Resume</a>
+			<a id="resume" href="/royi-katz-cv.pdf" download="royi-katz-cv.pdf">
+				<img src={downloadIcon} alt="Download" width={16} height={16} />
+				Resume
+			</a>
 			<a href="/contact">Contact</a>
 		</nav>
 	);
