95001a2a3c1a76d064d15f9baac234f79daed821
RoyiKatz1 <royicats@gmail.com>
2025-03-02 20:25:20 +0200

    Fix GitHub Pages routing issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	useNavigate,
+	useLocation,
+} from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import CaseStudyW4MOnboarding from "./pages/CaseStudyW4MOnboarding";
 import CaseStudyW4MRecruiter from "./pages/CaseStudyW4MRecruiter";
 import Navbar from "./components/Navbar";
 
+function RedirectHandler() {
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	useEffect(() => {
+		// If we were redirected (URL contains "?/"), fix the route
+		if (window.location.search.startsWith("?/")) {
+			const newPath = window.location.search.replace("?/", "");
+			navigate(newPath, { replace: true });
+		}
+	}, [location]);
+
+	return null;
+}
+
 function App() {
 	return (
 		<Router>
+			<RedirectHandler /> {/* Handles GitHub Pages redirects */}
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
